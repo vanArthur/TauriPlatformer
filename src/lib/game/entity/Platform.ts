33aee8d306c3c82e85da1894b98255ca74fefdde
vanArthur <vanackere.arthur@gmail.com>
2022-03-15 17:01:16 +0100

    Better export message

--- src/lib/game/entity/Platform.ts
+++ src/lib/game/entity/Platform.ts
@@ -2,14 +2,18 @@ import { Rectangle } from "../helperFunctions/shapes.js";
 import Entity from "./Entity.js";
 import { Vec2 } from "../helperFunctions/vector.js"
 
+
 export class Platform extends Entity {
     constructor(id: string, pos: Vec2, width: number, height: number, color: string) {
         super(id, pos, new Rectangle(0, 0, width, height, color));
         this.collider = new Rectangle(0, 0, width, height, color);
     }
     export() {
-        console.log(
-            `game.addPlatform(randomId(),new Vec2(${this.pos.x}, ${this.pos.y}), ${this.shapes[0].width}, ${this.shapes[0].height}, "${this.shapes[0].color}")`
-        );
+        let pf = {
+            id: this.id,
+            pos: this.pos,
+            shapes: this.shapes,
+        }
+        console.log(pf)
     }
 }
