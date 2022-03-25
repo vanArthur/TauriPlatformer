ada00eab8e23188a18407ebfbeb157c25b445ab6
vanArthur <vanackere.arthur@gmail.com>
2022-03-25 16:03:54 +0100

    Added settings menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import GameCanvas from "./components/game/GameCanvas";
+import { appWindow } from "@tauri-apps/api/window";
+import { Drawer } from "@mantine/core";
 
 function App() {
+  const [inGame, setinGame] = useState(false);
+  const [settingsPopUp, setsettingsPopUp] = useState(false);
+
   return (
-    <div>
-      <GameCanvas></GameCanvas>
-    </div>
+    <>
+      <Drawer
+        opened={settingsPopUp}
+        onClose={() => setsettingsPopUp(false)}
+        padding="xl"
+        size="xl"
+      >
+        <h1 className="bold">Settings</h1>
+      </Drawer>
+      <div style={{ display: "grid", placeContent: "center", height: "100vh" }}>
+        {inGame && !settingsPopUp && <GameCanvas></GameCanvas>}
+        {!inGame && !settingsPopUp && (
+          <>
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "darkgray",
+                borderRadius: "10px",
+                padding: "7px 15px 7px 15px",
+              }}
+              onClick={() => {
+                appWindow.setTitle("Loading Tauri Platformer");
+                setinGame(!inGame);
+              }}
+            >
+              Start
+            </button>
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "darkgray",
+                borderRadius: "10px",
+                padding: "7px 15px 7px 15px",
+              }}
+              onClick={() => {
+                setsettingsPopUp(!settingsPopUp);
+              }}
+            >
+              Settings
+            </button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
