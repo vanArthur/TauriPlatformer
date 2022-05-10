6b0edc3179a547578bb3c5e0e771bbc5e78ef960
vanArthur <vanackere.arthur@gmail.com>
2022-05-10 18:19:56 +0200

    remove all github actions/workflows

---
+++ packages/cli/rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+
+export default {
+  treeshake: true,
+  perf: true,
+  input: { index: "index.ts" },
+  output: {
+    dir: "dist",
+    format: "esm",
+    entryFileNames: "[name].js",
+    exports: "named",
+  },
+  plugins: [typescript({
+    allowSyntheticDefaultImports: true
+  })],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    "path",
+  ],
+  watch: {
+    chokidar: true,
+    include: "src/**",
+    exclude: "node_modules/**",
+  },
+};
